973b8bb1fbed24d2dc476d4cc2201aa33cf95c68
Ilya Shalyapin <ishalyapin@gmail.com>
2012-07-11 13:17:02 +0600

    ..

--- pencil.js
+++ pencil.js
@@ -8,8 +8,10 @@
 
     function Pencil(textarea, options){
 		this.$textarea = $(textarea);
-		$(textarea).css('color', '#999')
 
+		this.$textarea.wrap('<div class="pencil_wrapper"></div>')
+		this.$wrapper = $('.pencil_wrapper');
+		this.$wrapper.width(this.$textarea.width());
 
 		$(textarea).after('<div class="pencil_div" style="overflow-y: scroll;"></div>');
 		this.$div = $('.pencil_div');
@@ -18,10 +20,44 @@
         this.$div.width(this.$textarea.width());
         this.$div.height(this.$textarea.height());
 
-		$(textarea).before('<ul><a class="pencil_toolbar_bold" href="#">bold</a></ul>')
+		this.$textarea.before(
+			'<ul class="pencil_toolbar">\
+				<li><a class="pencil_toolbar_bold" href="#"></a></li>\
+				<li><a class="pencil_toolbar_italic" href="#"></a></li>\
+				<li><a class="pencil_toolbar_strike" href="#"></a></li>\
+				<li><a class="pencil_toolbar_underline" href="#"></a></li>\
+				<li><a class="pencil_toolbar_left" href="#"></a></li>\
+				<li><a class="pencil_toolbar_center" href="#"></a></li>\
+				<li><a class="pencil_toolbar_right" href="#"></a></li>\
+				<li><a class="pencil_toolbar_ol" href="#"></a></li>\
+				<li><a class="pencil_toolbar_ul" href="#"></a></li>\
+				<li><a class="pencil_toolbar_h1" href="#"></a></li>\
+				<li><a class="pencil_toolbar_h2" href="#"></a></li>\
+				<li><a class="pencil_toolbar_h3" href="#"></a></li>\
+				<li><a class="pencil_toolbar_image" href="#"></a></li>\
+				<li><a class="pencil_toolbar_link" href="#"></a></li>\
+				<li><a class="pencil_toolbar_video" href="#"></a></li>\
+			</ul>\
+			<div style="clear: left;"></div>'
+		)
+		this.$div.after(
+			'<ul class="pencil_switch">\
+				<li class="pencil_switch_html"><a href="#">Switch to HTMLMode</a></li>\
+				<li class="pencil_switch_visual"><a href="#">Switch to Visual Mode</a></li>\
+			</ul>'
+		)
 		$('.pencil_toolbar_bold').click(function(){
 			alert('ds');
-		})
+		});
+		_this = this;
+		$('.pencil_switch_html a').click(function(){
+			_this.htmlMode();
+			return false;
+		});
+		$('.pencil_switch_visual a').click(function(){
+			_this.visualMode();
+			return false;
+		});
 
 	}
 	Pencil.prototype = {
@@ -35,6 +71,10 @@
             this.$div.html(this.$textarea.val());
             this.$textarea.hide();
             this.$div.show();
+			$('.pencil_switch_visual').hide();
+			$('.pencil_switch_html').show();
+			$('.pencil_toolbar').show();
+
             this.mode = 'visual';
 		},
 		htmlMode: function(){
@@ -44,6 +84,9 @@
             this.$textarea.val(this.$div.html());
             this.$textarea.show();
             this.$div.hide();
+			$('.pencil_switch_html').hide();
+			$('.pencil_switch_visual').show();
+			$('.pencil_toolbar').hide();
 
             this.mode = 'html';
 		},
