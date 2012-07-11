dce7a4aedeb7151040c7628cbc0249127f2c6f86
Ilya Shalyapin <ishalyapin@gmail.com>
2012-07-12 00:23:39 +0600

    readme

--- pencil.js
+++ pencil.js
@@ -8,11 +8,6 @@ Browser compatibility: Firefox, Opera, Chome.
 Requirements:
 	- jQuery (http://jquery.com/)
 	- JQuery Form Plugin (http://malsup.com/jquery/form/)
-
-TODO:
-	- Debug image uploading
-	- Make possible to use 2 or more WYSIWYG in a page
-	- Test in IE
 */
 
 (function($){
